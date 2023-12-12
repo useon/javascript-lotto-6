381e4cf44b7e3240f5e620983a7c49bb75b46fe1
useon <yoosk4986@naver.com>
2023-12-12 16:51:06 +0900

    fix: call 하는 메서드에 인자를 추가로 전달하고 싶을 때 전달 안되는 문제 해결

--- src/utils/handlerErrorAndProceed.js
+++ src/utils/handlerErrorAndProceed.js
@@ -1,10 +1,10 @@
 import OutputView from '../views/OutputView.js';
 
-const handlerErrorAndProceed = async (method) => {
+const handlerErrorAndProceed = async (method, prams) => {
   let retryCount = 0;
   while (retryCount !== 10) {
     try {
-      return await method.call(this);
+      return await method.call(this, prams);
     } catch (error) {
       OutputView.printErrorMessage(error.message);
       retryCount += 1;
